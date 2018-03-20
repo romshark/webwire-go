68b8f1fc382ee6641ad5154ecffb223649c39c1f
Roman Sharkov <roman.sharkov@qbeon.com>
2018-03-20 17:44:54 +0100

    Add new integration test case
    Add a new integration test case to test user agent connectivity.

---
+++ test/clientAgentIsConnected_test.go
@@ -0,0 +1,93 @@
+package test
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	wwr "github.com/qbeon/webwire-go"
+	wwrclt "github.com/qbeon/webwire-go/client"
+)
+
+// TestClientAgentIsConnected tests the IsConnected method of the client agent
+func TestClientAgentIsConnected(t *testing.T) {
+	var clientAgent *wwr.Client
+	clientAgentDefined := NewPending(1, 1*time.Second, true)
+	clientDisconnected := NewPending(1, 1*time.Second, true)
+	testerGoroutineFinished := NewPending(1, 1*time.Second, true)
+
+	// Initialize webwire server
+	_, addr := setupServer(
+		t,
+		wwr.ServerOptions{
+			Hooks: wwr.Hooks{
+				OnClientConnected: func(newClt *wwr.Client) {
+					if !newClt.IsConnected() {
+						t.Errorf("Expected client agent to be connected")
+					}
+					clientAgent = newClt
+					clientAgentDefined.Done()
+
+					go func() {
+						if err := clientDisconnected.Wait(); err != nil {
+							t.Errorf("Client didn't disconnect")
+						}
+
+						if clientAgent.IsConnected() {
+							t.Errorf("Expected client agent to be disconnected")
+						}
+
+						testerGoroutineFinished.Done()
+					}()
+				},
+				OnClientDisconnected: func(clt *wwr.Client) {
+					if clientAgent.IsConnected() {
+						t.Errorf("Expected client agent to be disconnected")
+					}
+
+					// Try to send a signal to a disconnected client and expect an error
+					sigErr := clientAgent.Signal("", wwr.Payload{Data: []byte("testdata")})
+					if _, isDisconnErr := sigErr.(wwr.DisconnectedErr); !isDisconnErr {
+						t.Errorf(
+							"Expected a DisconnectedErr, got: %s | %s",
+							reflect.TypeOf(sigErr),
+							sigErr,
+						)
+					}
+
+					clientDisconnected.Done()
+				},
+			},
+		},
+	)
+
+	// Initialize client
+	client := wwrclt.NewClient(
+		addr,
+		wwrclt.Options{
+			DefaultRequestTimeout: 2 * time.Second,
+		},
+	)
+
+	if err := client.Connect(); err != nil {
+		t.Fatalf("Couldn't connect: %s", err)
+	}
+
+	// Wait for the client agent to be set by the OnClientConnected handler
+	if err := clientAgentDefined.Wait(); err != nil {
+		t.Fatalf("Tester goroutine didn't finish within 1 second")
+	}
+
+	if !clientAgent.IsConnected() {
+		t.Fatalf("Expected client agent to be connected")
+	}
+
+	// Close the client connection and continue in the tester goroutine
+	// spawned in the OnClientConnected handler of the server
+	client.Close()
+
+	// Wait for the tester goroutine to finish
+	if err := testerGoroutineFinished.Wait(); err != nil {
+		t.Fatalf("Tester goroutine didn't finish within 1 second")
+	}
+}
