cf33d92fe0633556dfe816b3af8e9126a7914380
Roman Sharkov <roman.sharkov@qbeon.com>
2018-08-14 15:50:27 +0200

    Refactor code
    Reduce complexity of the server-side signal testing code.

--- test/serverSignal_test.go
+++ test/serverSignal_test.go
@@ -19,42 +19,26 @@ func TestServerSignal(t *testing.T) {
 		wwr.EncodingBinary,
 		[]byte("webwire_test_SERVER_SIGNAL_payload"),
 	)
-	var addr string
-	serverSignalArrived := tmdwg.NewTimedWaitGroup(1, 1*time.Second)
-	initClient := make(chan bool, 1)
-	sendSignal := make(chan bool, 1)
+	signalProcessed := tmdwg.NewTimedWaitGroup(1, 1*time.Second)
 
 	// Initialize webwire server
-	go func() {
-		server := setupServer(
-			t,
-			&serverImpl{
-				onClientConnected: func(conn wwr.Connection) {
-					// Send signal
-					assert.NoError(t, conn.Signal(
-						"",
-						expectedSignalPayload,
-					))
-				},
+	server := setupServer(
+		t,
+		&serverImpl{
+			onClientConnected: func(conn wwr.Connection) {
+				// Send signal
+				assert.NoError(t, conn.Signal(
+					"",
+					expectedSignalPayload,
+				))
 			},
-			wwr.ServerOptions{},
-		)
-		addr = server.Addr().String()
-
-		// Synchronize, initialize client
-		initClient <- true
-
-		// Synchronize, wait for the client to launch
-		// and require the signal to be sent
-		<-sendSignal
-	}()
-
-	// Synchronize, await server initialization
-	<-initClient
+		},
+		wwr.ServerOptions{},
+	)
 
 	// Initialize client
 	client := newCallbackPoweredClient(
-		addr,
+		server.Addr().String(),
 		wwrclt.Options{
 			DefaultRequestTimeout: 2 * time.Second,
 		},
@@ -64,7 +48,7 @@ func TestServerSignal(t *testing.T) {
 				comparePayload(t, expectedSignalPayload, signalPayload)
 
 				// Synchronize, unlock main goroutine to pass the test case
-				serverSignalArrived.Progress(1)
+				signalProcessed.Progress(1)
 			},
 		},
 	)
@@ -73,13 +57,9 @@ func TestServerSignal(t *testing.T) {
 	// Connect client
 	require.NoError(t, client.connection.Connect())
 
-	// Synchronize, notify the server the client was initialized
-	// and request the signal
-	sendSignal <- true
-
 	// Synchronize, await signal arrival
 	require.NoError(t,
-		serverSignalArrived.Wait(),
+		signalProcessed.Wait(),
 		"Server signal didn't arrive",
 	)
 }
