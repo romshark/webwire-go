08e4a4a01e8cba74d9cea3fd783954e4d7710b38
Roman Sharkov <roman.sharkov@qbeon.com>
2018-09-11 23:29:41 +0200

    Add test
    Add test testing a simple server shutdown with only just connected
    clients without any pending tasks.

---
+++ test/simpleShutdown_test.go
@@ -0,0 +1,39 @@
+package test
+
+import (
+	"testing"
+
+	wwr "github.com/qbeon/webwire-go"
+	wwrclt "github.com/qbeon/webwire-go/client"
+	"github.com/stretchr/testify/require"
+)
+
+// TestSimpleShutdown tests simple shutdown without any pending tasks
+func TestSimpleShutdown(t *testing.T) {
+	connectedClientsNum := 5
+
+	// Initialize webwire server
+	server := setupServer(
+		t,
+		&serverImpl{},
+		wwr.ServerOptions{},
+	)
+
+	serverAddr := server.Addr().String()
+
+	clients := make([]*callbackPoweredClient, connectedClientsNum)
+	for i := 0; i < connectedClientsNum; i++ {
+		client := newCallbackPoweredClient(
+			serverAddr,
+			wwrclt.Options{
+				Autoconnect: wwr.Disabled,
+			},
+			callbackPoweredClientHooks{},
+		)
+		require.NoError(t, client.connection.Connect())
+		defer client.connection.Close()
+		clients[i] = client
+	}
+
+	require.NoError(t, server.Shutdown())
+}
