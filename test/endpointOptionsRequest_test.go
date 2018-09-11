019d23fdad6011796eb9582dc524f30e4772af5d
Roman Sharkov <roman.sharkov@qbeon.com>
2018-09-11 22:00:18 +0200

    Add test
    Add test testing OPTIONS HTTP request on the webwire endpoint.

---
+++ test/endpointOptionsRequest_test.go
@@ -0,0 +1,39 @@
+package test
+
+import (
+	"net/http"
+	"testing"
+	"time"
+
+	wwr "github.com/qbeon/webwire-go"
+	"github.com/stretchr/testify/require"
+)
+
+// TestEndpointOptions tests server endpoint using the OPTIONS method
+func TestEndpointOptions(t *testing.T) {
+	// Initialize webwire server
+	server := setupServer(t, &serverImpl{}, wwr.ServerOptions{})
+
+	// Initialize HTTP client
+	var httpClient = &http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	// Request metadata
+	request, err := http.NewRequest(
+		"OPTIONS",
+		"http://"+server.Addr().String()+"/",
+		nil,
+	)
+	require.NoError(t, err)
+	response, err := httpClient.Do(request)
+	require.NoError(t, err)
+
+	require.Equal(
+		t,
+		"WEBWIRE",
+		response.Header.Get("Access-Control-Allow-Methods"),
+	)
+	require.Equal(t, "*", response.Header.Get("Access-Control-Allow-Origin"))
+	require.Equal(t, "0", response.Header.Get("Content-Length"))
+}
