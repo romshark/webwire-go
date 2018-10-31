5aba811223eaed64ab5e4a04d12e0c8711d64b27
Roman Sharkov <roman.sharkov@qbeon.com>
2018-10-31 21:23:36 +0100

    Add request benchmark

---
+++ test/benchmarkUtil.go
@@ -0,0 +1,70 @@
+package test
+
+import (
+	"context"
+	"fmt"
+
+	wwr "github.com/qbeon/webwire-go"
+	"github.com/valyala/fasthttp"
+)
+
+// setupBenchmarkServer helps setting up and launching the benchmark server
+// together with the hosting HTTP(S) server
+func setupBenchmarkServer(
+	implementation *serverImpl,
+	options wwr.ServerOptions,
+) wwr.Server {
+	if implementation.beforeUpgrade == nil {
+		implementation.beforeUpgrade = func(
+			_ *fasthttp.RequestCtx,
+		) wwr.ConnectionOptions {
+			return wwr.ConnectionOptions{}
+		}
+	}
+	if implementation.onClientConnected == nil {
+		implementation.onClientConnected = func(_ wwr.Connection) {}
+	}
+	if implementation.onClientDisconnected == nil {
+		implementation.onClientDisconnected = func(_ wwr.Connection) {}
+	}
+	if implementation.onSignal == nil {
+		implementation.onSignal = func(
+			_ context.Context,
+			_ wwr.Connection,
+			_ wwr.Message,
+		) {
+		}
+	}
+	if implementation.onRequest == nil {
+		implementation.onRequest = func(
+			_ context.Context,
+			_ wwr.Connection,
+			_ wwr.Message,
+		) (response wwr.Payload, err error) {
+			return nil, nil
+		}
+	}
+
+	// Use default session manager if no specific one is defined
+	if options.SessionManager == nil {
+		options.SessionManager = newInMemSessManager()
+	}
+
+	// Use default localhost address
+	options.Host = "127.0.0.1:0"
+
+	server, err := wwr.NewServer(implementation, options)
+	if err != nil {
+		panic(fmt.Errorf("failed setting up server instance: %s", err))
+	}
+
+	// Run the server in a separate goroutine
+	go func() {
+		if err := server.Run(); err != nil {
+			panic(fmt.Errorf("server failed: %s", err))
+		}
+	}()
+
+	// Return reference to the server and the address its bound to
+	return server
+}
---
+++ test/benchmark_test.go
@@ -0,0 +1,195 @@
+package test
+
+import (
+	"context"
+	"testing"
+
+	wwr "github.com/qbeon/webwire-go"
+	wwrclt "github.com/qbeon/webwire-go/client"
+)
+
+// BenchmarkRequestC1_P1K benchmarks a request with a 1 kb payload on a single
+// connection
+func BenchmarkRequestC1_P1K(b *testing.B) {
+	// Preallocate the payload
+	payloadData := make([]byte, 1024)
+	msg := wwr.NewPayload(
+		wwr.EncodingUtf8,
+		payloadData,
+	)
+
+	// Initialize a webwire server
+	server := setupBenchmarkServer(
+		&serverImpl{
+			onRequest: func(
+				_ context.Context,
+				conn wwr.Connection,
+				msg wwr.Message,
+			) (wwr.Payload, error) {
+				return msg.Payload(), nil
+			},
+		},
+		wwr.ServerOptions{},
+	)
+
+	// Initialize client
+	client := newCallbackPoweredClient(
+		server.AddressURL(),
+		wwrclt.Options{},
+		callbackPoweredClientHooks{},
+	)
+
+	if err := client.connection.Connect(); err != nil {
+		panic(err)
+	}
+
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		client.connection.Request(context.Background(), "", msg)
+	}
+}
+
+// BenchmarkRequestC1_P1M benchmarks a request with a 1 mb payload on a single
+// connection
+func BenchmarkRequestC1_P1M(b *testing.B) {
+	// Preallocate the payload
+	payloadData := make([]byte, 1024*1024*1024)
+	msg := wwr.NewPayload(
+		wwr.EncodingUtf8,
+		payloadData,
+	)
+
+	// Initialize a webwire server
+	server := setupBenchmarkServer(
+		&serverImpl{
+			onRequest: func(
+				_ context.Context,
+				conn wwr.Connection,
+				msg wwr.Message,
+			) (wwr.Payload, error) {
+				return msg.Payload(), nil
+			},
+		},
+		wwr.ServerOptions{},
+	)
+
+	// Initialize client
+	client := newCallbackPoweredClient(
+		server.AddressURL(),
+		wwrclt.Options{},
+		callbackPoweredClientHooks{},
+	)
+
+	if err := client.connection.Connect(); err != nil {
+		panic(err)
+	}
+
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		client.connection.Request(context.Background(), "", msg)
+	}
+}
+
+// BenchmarkRequestC1_P1MBuffered benchmarks a request with a 1 mb payload on a
+// single connection
+func BenchmarkRequestC1_P1MBuffered(b *testing.B) {
+	// Preallocate the payload
+	payloadData := make([]byte, 1024*1024)
+	msg := wwr.NewPayload(
+		wwr.EncodingUtf8,
+		payloadData,
+	)
+
+	const bufferSize uint = 2 * 1024 * 1024
+
+	// Initialize a webwire server
+	server := setupBenchmarkServer(
+		&serverImpl{
+			onRequest: func(
+				_ context.Context,
+				conn wwr.Connection,
+				msg wwr.Message,
+			) (wwr.Payload, error) {
+				return msg.Payload(), nil
+			},
+		},
+		wwr.ServerOptions{
+			WriteBufferSize: bufferSize,
+			ReadBufferSize:  bufferSize,
+		},
+	)
+
+	// Initialize client
+	client := newCallbackPoweredClient(
+		server.AddressURL(),
+		wwrclt.Options{
+			WriteBufferSize: bufferSize,
+			ReadBufferSize:  bufferSize,
+		},
+		callbackPoweredClientHooks{},
+	)
+
+	if err := client.connection.Connect(); err != nil {
+		panic(err)
+	}
+
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		client.connection.Request(context.Background(), "", msg)
+	}
+}
+
+// BenchmarkRequestC1K_P1K benchmarks a request with a 1 kb payload on 1000
+// concurrent connections
+func BenchmarkRequestC1K_P1K(b *testing.B) {
+	concurrentConnections := 1000
+
+	// Preallocate the payload
+	payloadData := make([]byte, 1024)
+	msg := wwr.NewPayload(
+		wwr.EncodingUtf8,
+		payloadData,
+	)
+
+	// Initialize a webwire server
+	server := setupBenchmarkServer(
+		&serverImpl{
+			onRequest: func(
+				_ context.Context,
+				conn wwr.Connection,
+				msg wwr.Message,
+			) (wwr.Payload, error) {
+				return msg.Payload(), nil
+			},
+		},
+		wwr.ServerOptions{},
+	)
+
+	// Initialize client
+	clients := make([]*callbackPoweredClient, concurrentConnections)
+	for i := 0; i < concurrentConnections; i++ {
+		client := newCallbackPoweredClient(
+			server.AddressURL(),
+			wwrclt.Options{},
+			callbackPoweredClientHooks{},
+		)
+		clients[i] = client
+		if err := client.connection.Connect(); err != nil {
+			panic(err)
+		}
+	}
+
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		for _, c := range clients {
+			client := c
+			go func() {
+				client.connection.Request(context.Background(), "", msg)
+			}()
+		}
+	}
+}
