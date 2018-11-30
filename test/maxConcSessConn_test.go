257ba923d1d71c2bd2f452078f764c6497ee228a
Roman Sharkov <roman.sharkov@qbeon.com>
2018-11-30 12:28:31 +0100

    Fix and simplify test
    Fix and simplify the per-session concurrent connections limit test.

--- test/maxConcSessConn_test.go
+++ test/maxConcSessConn_test.go
@@ -2,7 +2,6 @@ package test
 
 import (
 	"context"
-	"sync"
 	"testing"
 	"time"
 
@@ -15,67 +14,46 @@ import (
 
 // TestMaxConcSessConn tests 4 maximum concurrent connections of a session
 func TestMaxConcSessConn(t *testing.T) {
-	sessionStorage := make(map[string]*wwr.Session)
-
-	var sessionKey string
-	sessionKeyLock := sync.RWMutex{}
 	concurrentConns := uint(4)
+
+	var sessionKey = "testsessionkey"
+	sessionCreation := time.Now()
 
 	// Initialize server
 	setup := setupTestServer(
 		t,
-		&serverImpl{
-			onClientConnected: func(
-				_ wwr.ConnectionOptions,
-				conn wwr.Connection,
-			) {
-				// Created the session for the first connecting client only
-				sessionKeyLock.Lock()
-				defer sessionKeyLock.Unlock()
-				if len(sessionKey) < 1 {
-					assert.NoError(t, conn.CreateSession(nil))
-					sessionKey = conn.SessionKey()
-				}
-			},
-		},
+		&serverImpl{},
 		wwr.ServerOptions{
 			MaxSessionConnections: concurrentConns,
 			SessionManager: &callbackPoweredSessionManager{
-				// Saves the session
-				SessionCreated: func(conn wwr.Connection) error {
-					sess := conn.Session()
-					sessionStorage[sess.Key] = sess
-					return nil
-				},
-				// Finds session by key
 				SessionLookup: func(key string) (
 					webwire.SessionLookupResult,
 					error,
 				) {
-					if session, exists := sessionStorage[key]; exists {
-						return webwire.NewSessionLookupResult(
-							session.Creation,   // Creation
-							session.LastLookup, // LastLookup
-							webwire.SessionInfoToVarMap(
-								session.Info,
-							), // Info
-						), nil
+					if key != sessionKey {
+						// Session not found
+						return nil, nil
 					}
-					// Session not found
-					return nil, nil
+					return webwire.NewSessionLookupResult(
+						sessionCreation, // Creation
+						time.Now(),      // LastLookup
+						nil,             // Info
+					), nil
 				},
 			},
 		},
 		nil, // Use the default transport implementation
 	)
 
-	// Initialize client
+	clientOptions := wwrclt.Options{
+		DefaultRequestTimeout: 2 * time.Second,
+	}
+
+	// Initialize clients
 	clients := make([]*testClient, concurrentConns)
 	for i := uint(0); i < concurrentConns; i++ {
 		client := setup.newClient(
-			wwrclt.Options{
-				DefaultRequestTimeout: 2 * time.Second,
-			},
+			clientOptions,
 			nil, // Use the default transport implementation
 			testClientHooks{},
 		)
@@ -83,22 +61,16 @@ func TestMaxConcSessConn(t *testing.T) {
 
 		assert.NoError(t, client.connection.Connect())
 
-		// Restore the session for all clients except the first one
-		if i > 0 {
-			sessionKeyLock.RLock()
-			assert.NoError(t, client.connection.RestoreSession(
-				context.Background(),
-				[]byte(sessionKey),
-			))
-			sessionKeyLock.RUnlock()
-		}
+		// Restore the session for all clients
+		assert.NoError(t, client.connection.RestoreSession(
+			context.Background(),
+			[]byte(sessionKey),
+		))
 	}
 
 	// Ensure that the last superfluous client is rejected
 	superfluousClient := setup.newClient(
-		wwrclt.Options{
-			DefaultRequestTimeout: 2 * time.Second,
-		},
+		clientOptions,
 		nil, // Use the default transport implementation
 		testClientHooks{},
 	)
@@ -107,12 +79,10 @@ func TestMaxConcSessConn(t *testing.T) {
 
 	// Try to restore the session and expect this operation to fail
 	// due to reached limit
-	sessionKeyLock.RLock()
 	sessionRestorationError := superfluousClient.connection.RestoreSession(
 		context.Background(),
 		[]byte(sessionKey),
 	)
 	require.Error(t, sessionRestorationError)
 	require.IsType(t, wwr.MaxSessConnsReachedErr{}, sessionRestorationError)
-	sessionKeyLock.RUnlock()
 }
